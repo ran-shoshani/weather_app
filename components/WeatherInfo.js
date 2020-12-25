ee74d05fdcf2cea52b1626b120e06cadde2f13e6
redtamuchi <6369@ait.nsw.edu.au>
2020-12-25 20:46:13 +1100

    adding city icon temp weather-description

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { View, Text ,StyleSheet} from 'react-native'
+import { View, Text ,StyleSheet , Image} from 'react-native'
 
 export default function WeatherInfo({currentWeather}) {
-    const { main: { temp }, wether: [details]} = currentWeather
+    const { main: { temp }, weather: [details] , name , } = currentWeather
     
-    const {icon} = details
-    const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
+    const {icon,main, description} = details
+    // icon from open-weather as an array
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@3x.png`
     return (
-        <View>
+        <View style={styles.WeatherInfo}>
+            <Text>{name}</Text>
+            <Image source = {{ uri: iconUrl }}/>
             <Text>{temp}</Text>
+            <Text>{description}</Text>
+            <Text>{main}</Text>
+
         </View>
     )
 }
@@ -16,5 +22,9 @@ export default function WeatherInfo({currentWeather}) {
 const styles = StyleSheet.create({
     WeatherInfo: {
         alignItems: 'center'
-    }
+    },
+    weatherIcon: {
+        width: 100,
+        height: 100,
+    },
 })
