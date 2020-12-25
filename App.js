836487dc8814afe1248304c9574a021bfc189d8f
redtamuchi <6369@ait.nsw.edu.au>
2020-12-25 18:43:01 +1100

    expo added and location

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
+
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitsSystem, setUnitsSystem] = useState('metric')
+
+  useEffect(() => {
+    load()
+  }, [unitsSystem])
+  async function load(){
+    try {
+      let { status } = await Location.requestPermissionsAsync()
+      if (status !== 'granted'){
+       setErrorMessage('access to location is needed!!!')
+       return
+      }
+      const location = await Location.getCurrentPositionAsync()
+
+      const {latitude, longitude} = location.coords
+      alert('Latitude : ${latitude}, Longitude: ${longitude}')
+    } catch (error) {}
+
+    
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello weather app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +39,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
